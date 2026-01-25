0b89297afe4941d648eba3855c92e89175c108ac
jordi-bujaldon <jordi.bujaldon@basetis.com>
2026-01-25 10:56:13 +0100

    Expose AppViewModel through AppViewModelHelper for iOS
    - Update `AppViewModelHelper` to inject and provide access to `AppViewModel`.
    - Add dependency on `:shared:common` in `:shared:ui` to support `AppViewModel` usage.

--- shared/ui/src/iosMain/kotlin/com/casavirupa/voluntariat/shared/ui/di/AppViewModelHelper.kt
+++ shared/ui/src/iosMain/kotlin/com/casavirupa/voluntariat/shared/ui/di/AppViewModelHelper.kt
@@ -1,6 +1,11 @@
 package com.casavirupa.voluntariat.shared.ui.di
 
+import com.casavirupa.voluntariat.shared.common.AppViewModel
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class AppViewModelHelper : KoinComponent {
+    private val viewModel: AppViewModel by inject()
+
+    fun getViewModel(): AppViewModel = viewModel
 }
