d4d2fa6161038cebfda55ac6becb4daab8070865
jordi-bujaldon <jordi.bujaldon@basetis.com>
2026-03-21 19:18:31 +0100

    Add `CalendarConstants` with Google Apps Script URL
    - Create `CalendarConstants.kt` in the `:shared:core` module.
    - Define `SCRIPT_URL` within `CalendarConstants` to store the Google Apps Script execution endpoint.

---
+++ shared/core/src/commonMain/kotlin/com/casavirupa/voluntariat/shared/core/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.casavirupa.voluntariat.shared.core.constants
+
+internal object CalendarConstants {
+    const val SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyFD8w7p4Z5F5jVw-" +
+            "4obwKq2dipZoo8rbZU1Dhl_SFByWLSQc_bterIcTiAHTjtoCT1/exec"
+}
