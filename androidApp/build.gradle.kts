65b0d02c317be7a453b8b9a696e5ed8b3cb9cceb
Jordi Bujaldon <jordi.bujaldon@basetis.com>
2026-09-24 20:10:11 +0200

    Bump app version to 1.1.2
    - Update Android `versionName` to `1.1.2` in `androidApp/build.gradle.kts`.
    - Update iOS `MARKETING_VERSION` to `1.1.2` and change `CURRENT_PROJECT_VERSION` to `1` in the Xcode project configuration.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.casavirupa.voluntariat.android"
         versionCode = 1
-        versionName = "1.0.11"
+        versionName = "1.1.2"
     }
 
     buildTypes {
